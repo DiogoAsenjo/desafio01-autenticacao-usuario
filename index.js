c5bfbe6f5ccf55699f4b23ba570542ee49195b9f
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-02 10:40:35 -0300

    Refatorando a função de validação de usuário e senha

--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const dadosUsuarios = require("./usuarios.json");
 
 //Como não teremos front end no Desafio, criei duas variáveis para simular a tentativa de login de um usuário.
 const login = "diogo.asenjo@modalgr.com.br";
-const senha = "diogo123";
+const senha = "diogo12";
 
 const validandoUsuarioESenha = (lista, chave, valor) => {
     if (lista.find((item) => item[chave].includes(valor))) {
         let index = dadosUsuarios.findIndex((item) => item.usuario === valor);
-        return dadosUsuarios[index].senha === senha ? true : false;
+        if(dadosUsuarios[index].senha === senha) {
+            console.log(`Usuário logado com sucesso`);
+            return true;
+        } else {
+            console.log(`Usuario ou senha incorreto(s)`);
+            return false;
+        }
     } else {
+        console.log(`Usuario ou senha incorreto(s)`);
         return false;
     }
 }
 
-//Mensagem de sucesso ou falha no login.
-if(validandoUsuarioESenha(dadosUsuarios, "usuario", login)) {
-    console.log(`Usuário logado com sucesso`);
-} else {
-    console.log(`Usuario ou senha incorreto(s)`);
-}
+console.log(validandoUsuarioESenha(dadosUsuarios, "usuario", login));
 
 const verificacaoSenhaSegura = (senha) => {
     if(senha.length < 8) {
